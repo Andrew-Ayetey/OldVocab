449368e3ac6ef87f663d8b940eff3e85437d3ca4
HalfricanMan <44378844+HalfricanMan@users.noreply.github.com>
2018-12-21 12:02:01 -0600

    Add files via upload

---
+++ VBInsert.py
@@ -0,0 +1,59 @@
+from VocabFinder import *
+import sqlite3
+import pandas as pd
+pd.set_option('display.max_colwidth', -1)
+# pd.set_option('display.max_columns', None) 
+
+
+conn = sqlite3.connect('VBtest.db')
+
+c = conn.cursor()
+
+# print "".join(str((dictionary.meaning('weft').values())))
+
+
+
+
+
+
+
+# c.execute("""CREATE TABLE Definitions (
+# 	word text UNIQUE,
+#  	definition text UNIQUE
+#   	)""")
+
+def VBinsert(x):
+	defin = str(dictionary.meaning(x))
+	if defin == 'None':
+		return 'No definition'
+	else:
+		pass
+	
+	c.execute("INSERT INTO Definitions VALUES (?, ?)", (x, defin))
+	conn.commit()
+
+	return
+
+
+
+
+
+# VBinsert('ratiocination')
+# c.execute("INSERT INTO Definitions VALUES ('weft'), dictionary)
+
+# c.execute("SELECT * FROM Definitions")
+# print pd.read_sql_query("SELECT * FROM Definitions", conn)
+
+w = ['scurrilous']
+
+for x in w:
+	VBinsert(x)
+
+
+conn.commit()
+
+c.execute("SELECT * FROM Definitions")
+print pd.read_sql_query("SELECT * FROM Definitions", conn)
+
+conn.close()
+
---
+++ VocabFinder.py
@@ -0,0 +1,19 @@
+from PyDictionary import PyDictionary
+dictionary=PyDictionary()
+
+def FindWord(x):
+	defi = []
+	for y in x:
+		defi.append(dictionary.meaning(y))
+	return defi
+
+	
+
+# print FindWord(["arraign"])
+
+
+
+
+
+
+
