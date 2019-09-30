00510e926d6f23dac5a434b8520b191964b425ee
Andrew Ayetey <ayeteya@gmail.com>
2019-09-30 17:38:32 -0500

    Added Vocab Class

---
+++ vocab_word.py
@@ -0,0 +1,22 @@
+from PyDictionary import PyDictionary
+pydic = PyDictionary()
+
+class Vocab:
+	def __init__(self, word, definition=None, syn=None, antm=None):
+		self.word = word
+		self.definition = definition
+		self.syn = syn
+		self.antm= antm
+
+V1= Vocab("Pizza")
+
+class PyDic(Vocab):
+	def __init__(self, word, definition=None, syn=None, antm=None):
+		self.word = word
+		self.definition = pydic.meaning(word)
+		self.syn= pydic.synonym(word)
+		self.antm= pydic.antonym(word)
+
+Life = PyDic("Life")
+
+print(Life.word, Life.definition)
